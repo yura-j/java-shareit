34616579d67d30f57d9eeeee87f668aee2074fca
_yura <javadevelope@yandex.ru>
2022-10-13 16:50:59 +0300

    restart

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE IF NOT EXISTS items (
   description VARCHAR(512) NOT NULL,
   is_available BOOLEAN NOT NULL,
   owner_id BIGINT NOT NULL,
-  request_id BIGINT NOT NULL,
+  request_id BIGINT,
 
   CONSTRAINT pk_item PRIMARY KEY (id),
   CONSTRAINT owner_fk
