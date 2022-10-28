cbce2f65b05cc2109a08988cc560d767c7fb07f6
_yura <javadevelope@yandex.ru>
2022-10-28 23:50:16 +0300

    final fix

--- server/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
